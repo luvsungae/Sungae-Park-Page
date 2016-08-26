59229596a7b143aa27067ff1505dc122a2c09f43
Sungae Park <luvsungae@hotmail.com>
2016-08-26 04:32:01 +0000

    now

--- app.js
+++ app.js
@@ -33,18 +33,6 @@ app.get('/portfolios',function(req,res){
   res.sendFile('/portfolios.html');
 });
 
-var request = require('request');
-var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22fairfax%2C%20va%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithke';
-
-request(url, function (err, resp, body) {
-    if (err || resp.statusCode != 200)
-        return console.log('Could not get weather information');
-    var json = JSON.parse(body);
-    console.log('Five-day forecast for McLean, VA:', json.query.results.channel.item.condition.temp);
-});
-
-
-
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
